2a0fb3dc882fea999af17f65a226b19bcc80c13e
Khawlah Khalid <khawlah1920@hotmail.com>
2023-07-10 07:37:55 +0300

    Initial Commit

---
+++ TextAndLanguageML/TextAndLanguageMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextAndLanguageMLApp.swift
+//  TextAndLanguageML
+//
+//  Created by Khawlah Khalid on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TextAndLanguageMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
