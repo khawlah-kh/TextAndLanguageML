fddcb3b00cad1b6737e82889f4a97a70bba521a6
Khawlah Khalid <khawlah1920@hotmail.com>
2023-07-13 17:56:56 +0300

    Language identification

--- TextAndLanguageML/ContentView.swift
+++ TextAndLanguageML/ContentView.swift
@@ -6,18 +6,66 @@
 //
 
 import SwiftUI
+import NaturalLanguage
 
 struct ContentView: View {
+    @State var input: String = ""
+    @State var result: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 16) {
+            TextField("Type anything . . .", text: $input, axis: .vertical)
+                .font(.headline)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                predictLanguage()
+            } label: {
+                HStack{
+                    Spacer()
+                    Text("Predict language")
+                        .foregroundColor(.white)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                .background {
+                    Color.purple
+                }.cornerRadius(8)
+            }
+
+            Text("Result: \(result)")
+                .bold()
         }
         .padding()
     }
+    
+    
+
+    func predictLanguage(){
+        let locale = Locale(identifier: "en")
+        let recognizer = NLLanguageRecognizer()
+        recognizer.processString(input)
+        
+        guard let language = recognizer.dominantLanguage else {
+            result = "Unknown language"
+            return
+        }
+        
+        result = locale.localizedString(forLanguageCode: language.rawValue)
+        ?? "Unknown language Code"
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
